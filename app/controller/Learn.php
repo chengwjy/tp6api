3428055f3185489b831c9cbff25802effdf1715e
chenglin <896661698@qq.com>
2020-08-24 21:53:13 +0800

    add

---
+++ app/controller/Learn.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\controller;
+
+use app\BaseController;
+use app\Request;
+class Learn
+{
+    // 不继承BaseController而使用依赖注入的方式使用Request,不建议
+    public function index(Request $request)
+    {
+        var_dump($request->param());
+
+    }
+
+}
